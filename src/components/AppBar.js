c14291775242b6cc5da08d5693cb6c868e18fb2f
Sanjeev Kumar <san74kr@gmail.com>
2024-03-05 08:37:34 +0000

    "Resolve appbar merge conflict"

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -4,13 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { CgProfile } from "react-icons/cg";
 import exlLogo from "../assets/EXL_logo.png";
 import "../../src/components/Header/header.css";
-import exlLogo from "../assets/EXL_logo.png";
-import "../../src/components/Header/header.css";
 
-// import MuiAppBar from "@mui/material/AppBar";
-// import Toolbar from "@mui/material/Toolbar";
-// import Typography from "@mui/material/Typography";
-// import Container from "@mui/material/Container";
 
 export const AppBar = () => {
   const navigate = useNavigate();
@@ -45,42 +39,5 @@ export const AppBar = () => {
         </div>
       )}
     </>
-
-    // <MuiAppBar position="static" >
-    //   <Container maxWidth="xl" className="header-main-container">
-    //     <Toolbar disableGutters>
-    //       <Typography
-    //         variant="h6"
-    //         noWrap
-    //         component="div"
-    //         sx={{ mr: 2, display: { xs: "none", md: "flex", justifyContent: "space-between" } }}
-    //       >
-    //         {/* React Router Auth */}
-    //       </Typography>
-
-    //       {/*  <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-    //         <IconButton
-    //           size="large"
-    //           aria-label="account of current user"
-    //           aria-controls="menu-appbar"
-    //           aria-haspopup="true"
-
-    //           color="inherit"
-    //         >
-    //           <MenuIcon />
-    //         </IconButton>
-    //       </Box> */}
-    //       <Typography
-    //         variant="h6"
-    //         noWrap
-    //         component="div"
-    //         sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
-    //       >
-    //         React Router Auth
-    //       </Typography>
-
-    //     </Toolbar>
-    //   </Container>
-    // </MuiAppBar>
   );
 };
